debc0eba5da4506fddb196d4c0cb15e9a6950fe5
BaizaOP <zayn.baig@gmail.com>
2021-07-01 01:39:02 -0400

    added first part of problem 1 and a .gitignore

---
+++ problem1/hello.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "What is your name? ";
+    cin >> name;
+    string greeting = "Hello, " + name + ", nice to meet you!";
+    cout << greeting << endl;
+
+    return 0;
+}
